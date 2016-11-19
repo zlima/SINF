322dd013d93cffc36fb215a656b93c1134332a97
zlima <jlima91@hotmail.com>
2016-11-19 01:11:04 +0000

    first commit

---
+++ FirstREST/Controllers/TopVendasController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using FirstREST.Lib_Primavera.Model;
+
+namespace FirstREST.Controllers
+{
+
+    public class TopVendasController : ApiController
+    {
+        //
+        // GET: /TopVendas/
+
+        public IEnumerable<Lib_Primavera.Model.TopVenda> Get(long nr)
+        {
+            return Lib_Primavera.PriIntegration.ListaTopVenda(nr);
+        }
+    }
+}
+
---
+++ FirstREST/Lib_Primavera/Model/Fornecedor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstREST.Lib_Primavera.Model
+{
+    public class Fornecedor
+    {
+        public string CodFornecedor
+        {
+            get;
+            set;
+        }
+
+        public string Nome
+        {
+            get;
+            set;
+        }
+
+        public string Moeda
+        {
+            get;
+            set;
+        }
+
+        public string NumContrib
+        {
+            get;
+            set;
+        }
+
+        public string ModoPag
+        {
+            get;
+            set;
+        }
+
+        public string CondPag
+        {
+            get;
+            set;
+        }
+
+        public double TotalDeb
+        {
+            get;
+            set;
+        }
+
+    }
+}
